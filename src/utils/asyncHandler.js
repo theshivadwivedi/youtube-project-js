c52f5df6834c6ef3c6023eac4e085117d7cd7144
theshivadwivedi <dwivedishiva2006@gmail.com>
2025-04-10 15:35:06 +0530

    utils code

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,26 @@
+const asyncHandler=(requesthandler)=>{
+    
+    (req,res,next)=>{
+        Promise.resolve(requesthandler(req,res,next)).
+        catch((err)=>next(err))
+    }
+}
+
+
+
+export {asyncHandler}
+
+//  +++++++++++++         2nd method  +++++++
+
+// const asyncHandler=(fn)=> async(req,res,next)=>{
+//     try {
+
+//         await fn(req,res,next)
+        
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success:false,
+//             message:error.message
+//         })
+//     }
+// }
