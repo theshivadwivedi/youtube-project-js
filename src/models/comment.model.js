4ba93dff2c9616b6344bec89f2b58c8c49a82c96
theshivadwivedi <dwivedishiva2006@gmail.com>
2025-06-28 20:09:13 +0530

    commit recently

--- src/models/comment.model.js
+++ src/models/comment.model.js
@@ -1,5 +1,5 @@
 import { timeStamp } from "console";
-import mongoose,{Schema, schema} from "mongoose";
+import mongoose,{Schema} from "mongoose";
 import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 import { type } from "os";
 
